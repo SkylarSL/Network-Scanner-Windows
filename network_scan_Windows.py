49f579bd37153dfac6d85ac5ca77a57847c6e8ad
SkylarSL <skylarsamsonliang@gmail.com>
2024-02-16 21:05:26 -0500

    Turning it into an automatic script so you do not have to look up your IP before running

--- network_scan_Windows.py
+++ network_scan_Windows.py
@@ -1,4 +1,6 @@
 from scapy.all import ARP, Ether, srp
+import subprocess
+import re
 
 # Function to scan the local network with specified IP subnet
 def scan(target_ip) -> None:
@@ -31,17 +33,38 @@ def scan(target_ip) -> None:
 
         print("{:17}    |{}".format(client["ip"], client["mac"]))
 
-
 def main():
 
-    ip = input("enter your local ip appended with //24: ")
+    internet_type = input("What is the type of connectivity you are using for the internet? (Wireless|Wired)")
 
-    if int(ip) == -1:
-        ip = "10.0.0.0/24"
-
-    target_ip = str(ip)
+    ip_config_output_raw = subprocess.run(["ipconfig"], capture_output=True)
+    ip_config_output = ip_config_output_raw.stdout.decode()
     
-    scan(target_ip)
+    if str(internet_type) == "Wireless":
+
+        connection_type = re.search("Wireless LAN adapter Wi-Fi(.|\n|\r)*", ip_config_output)
+        print(connection_type.group())
+        connection_type = connection_type.group()
+        wireless_ip = re.search("IPv4 Address.*([0-9]|[1-9][0-9]|[1-9][0-9][0-9])\.([0-9]|[1-9][0-9]|[1-9][0-9][0-9])\.([0-9]|[1-9][0-9]|[1-9][0-9][0-9])\.([0-9]|[1-9][0-9]|[1-9][0-9][0-9])", connection_type)
+        print(wireless_ip.group())
+
+    else:
+
+        connection_type = re.search("<>", ip_config_output)
+        print(connection_type.group())
+        connection_type = connection_type.group()
+        wireed_ip = re.search("IPv4 Address.*([0-9]|[1-9][0-9]|[1-9][0-9][0-9])\.([0-9]|[1-9][0-9]|[1-9][0-9][0-9])\.([0-9]|[1-9][0-9]|[1-9][0-9][0-9])\.([0-9]|[1-9][0-9]|[1-9][0-9][0-9])", connection_type)
+        print(wireed_ip.group())
+
+
+    #ip = input("enter your local ip appended with //24: ")
+
+    #if int(ip) == -1:
+    #    ip = "10.0.0.0/24"
+
+    #target_ip = str(ip)
+    
+    # scan(target_ip)
 
     return 0
 
