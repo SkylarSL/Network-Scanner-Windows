14264c4d5aa1a8d0abc33d7a12671ce73f96f718
SkylarSL <skylarsamsonliang@gmail.com>
2024-02-17 22:12:29 -0500

    Finished making script for wireless connections. Renamed variables. Added some comments

--- network_scan_Windows.py
+++ network_scan_Windows.py
@@ -3,10 +3,12 @@ import subprocess
 import re
 
 # Function to scan the local network with specified IP subnet
-def scan(target_ip) -> None:
+def scan(subnet_addr) -> None:
 
-    arp = ARP(pdst=target_ip)
+    # Make an ARP packet with the subnet IP range
+    arp = ARP(pdst=subnet_addr)
 
+    # Make an Ethernet packet with broadcast MAC to be sent to all hosts in local network
     ether = Ether(dst="ff:ff:ff:ff:ff:ff")
 
     # Layer the ethernet and arp packet
@@ -15,15 +17,15 @@ def scan(target_ip) -> None:
     # Now a list of pairs that is formatted (sent_packet, received_packet)
     result = srp(packets, timeout = 3)[0]
 
+    # Initial array for clients
     clients = []
 
+    # Add recieved items to a list
     for sent, received in result:
-
-        print(received[1].psrc, received[1].hwsrc)
-        
+        # print(received[1].psrc, received[1].hwsrc)
         clients.append({"ip": received.psrc, "mac": received.hwsrc})
 
-    print("target ip was: {}".format(target_ip))
+    print("target subnet was: {}".format(subnet_addr))
 
     print("Available devices in the network:")
 
@@ -39,32 +41,52 @@ def main():
 
     ip_config_output_raw = subprocess.run(["ipconfig"], capture_output=True)
     ip_config_output = ip_config_output_raw.stdout.decode()
+
+    subnet_addr = ""
     
     if str(internet_type) == "Wireless":
 
+        # Get the Wireless LAN adapter information
         connection_type = re.search("Wireless LAN adapter Wi-Fi(.|\n|\r)*", ip_config_output)
-        print(connection_type.group())
         connection_type = connection_type.group()
-        wireless_ip = re.search("IPv4 Address.*([0-9]|[1-9][0-9]|[1-9][0-9][0-9])\.([0-9]|[1-9][0-9]|[1-9][0-9][0-9])\.([0-9]|[1-9][0-9]|[1-9][0-9][0-9])\.([0-9]|[1-9][0-9]|[1-9][0-9][0-9])", connection_type)
-        print(wireless_ip.group())
+        # Get the IPv4 address associated with the Wireless LAN adapter
+        wireless_ip = re.search("IPv4 Address.*([1-9][0-9][0-9]|[1-9][0-9]|[0-9])\.([1-9][0-9][0-9]|[1-9][0-9]|[0-9])\.([1-9][0-9][0-9]|[1-9][0-9]|[0-9])\.([1-9][0-9][0-9]|[1-9][0-9]|[0-9])", connection_type)
+        wireless_ip = wireless_ip.group()
+        # Get the IP address
+        ip_addr = re.search("([1-9][0-9][0-9]|[1-9][0-9]|[0-9])\.([1-9][0-9][0-9]|[1-9][0-9]|[0-9])\.([1-9][0-9][0-9]|[1-9][0-9]|[0-9])\.([1-9][0-9][0-9]|[1-9][0-9]|[0-9])", wireless_ip)
+        ip_addr = ip_addr.group()
 
-    else:
+        # Turn found IP address into likely subnet address
+        ip_addr = ip_addr.split(".")
+        ip_addr[-1] = "0/24"
+        subnet_addr = ".".join(ip_addr)
 
+    elif str(internet_type) == "Wired":
+
+        # TBD
+        """
         connection_type = re.search("<>", ip_config_output)
         print(connection_type.group())
         connection_type = connection_type.group()
-        wireed_ip = re.search("IPv4 Address.*([0-9]|[1-9][0-9]|[1-9][0-9][0-9])\.([0-9]|[1-9][0-9]|[1-9][0-9][0-9])\.([0-9]|[1-9][0-9]|[1-9][0-9][0-9])\.([0-9]|[1-9][0-9]|[1-9][0-9][0-9])", connection_type)
-        print(wireed_ip.group())
+        wired_ip = re.search("IPv4 Address.*([0-9]|[1-9][0-9]|[1-9][0-9][0-9])\.([0-9]|[1-9][0-9]|[1-9][0-9][0-9])\.([0-9]|[1-9][0-9]|[1-9][0-9][0-9])\.([0-9]|[1-9][0-9]|[1-9][0-9][0-9])", connection_type)
+        print(wired_ip.group())
+        wired_ip = wired_ip.group()
+        # Get the IP address
+        ip_addr = re.search("([0-9]|[1-9][0-9]|[1-9][0-9][0-9])\.([0-9]|[1-9][0-9]|[1-9][0-9][0-9])\.([0-9]|[1-9][0-9]|[1-9][0-9][0-9])\.([0-9]|[1-9][0-9]|[1-9][0-9][0-9])", wired_ip)
+        """
 
+        subnet_addr  = -1
 
-    #ip = input("enter your local ip appended with //24: ")
+    else:
 
-    #if int(ip) == -1:
-    #    ip = "10.0.0.0/24"
+        subnet_addr  = -1
 
-    #target_ip = str(ip)
-    
-    # scan(target_ip)
+    # Error checking
+    if subnet_addr  == -1:
+        print("No Subnet found.")
+
+    # Run scan
+    scan(subnet_addr )
 
     return 0
 
